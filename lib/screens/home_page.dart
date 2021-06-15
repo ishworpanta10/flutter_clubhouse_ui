9e4223741f0a326ca29bbe6d9dd6d0922db33906
ishworpanta10 <ishworpanta10@gmail.com>
2021-06-15 21:20:46 +0545

    completed appbar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,57 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_clubhouse_ui/model/user.dart';
+import 'package:flutter_clubhouse_ui/widgets/widgets.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Clubhouse Ui Clone'),
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(
+            CupertinoIcons.search,
+            size: 28.0,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.envelope_open,
+              size: 26.0,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.calendar,
+              size: 28.0,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.bell,
+              size: 28.0,
+            ),
+            onPressed: () {},
+          ),
+          GestureDetector(
+            onTap: (){},
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 10.0, 20.0, 10.0),
+              child: UserProfileImage(
+                imageUrl: currentUser.imageUrl,
+                size: 36.0,
+              ),
+            ),
+          )
+        ],
       ),
       body: Center(
-        child: Text('Clubhouse ui'),
+        child: Text("Flutter "),
       ),
     );
   }
