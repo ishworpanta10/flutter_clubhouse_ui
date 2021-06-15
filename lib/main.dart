4adff2fe65bd0aaa8bdff3849692e014b6641a60
ishworpanta10 <ishworpanta10@gmail.com>
2021-06-15 19:53:41 +0545

    added fonts family and colors pallete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clubhouse_ui/screens/home_page.dart';
+import 'package:flutter_clubhouse_ui/style/color/custom_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Clubhouse UI Clone',
       theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: CustomColor.background,
+        ),
+        scaffoldBackgroundColor: CustomColor.background,
         primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        accentColor: Colors.green,
+        iconTheme: const IconThemeData(color: Colors.black),
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+        textTheme: GoogleFonts.montserratTextTheme(),
       ),
       home: HomePage(),
     );
