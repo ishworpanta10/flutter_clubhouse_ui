23009c926acbc2dafc1b02104598138cd25dacf8
ishworpanta10 <ishworpanta10@gmail.com>
2021-06-26 11:56:33 +0545

    completed homescreen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -52,12 +52,88 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: ListView(
-        padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 120.0),
+      body: Stack(
+        alignment: Alignment.center,
         children: [
-          UpcomingRooms(upcomingRooms: upcomingRoomsList),
-          const SizedBox(height: 12.0),
-          ...roomsList.map((room) => RoomCard(room: room)),
+          ListView(
+            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 120.0),
+            children: [
+              UpcomingRooms(upcomingRooms: upcomingRoomsList),
+              const SizedBox(height: 12.0),
+              ...roomsList.map((room) => RoomCard(room: room)),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 100.0,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [
+                  Theme.of(context).scaffoldBackgroundColor.withOpacity(0.1),
+                  Theme.of(context).scaffoldBackgroundColor,
+                ]),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 60.0,
+            child: Container(
+              padding: const EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.circular(24.0),
+              ),
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    WidgetSpan(
+                      child: Icon(
+                        CupertinoIcons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' Start a room',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 60,
+            right: 40.0,
+            child: Stack(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    CupertinoIcons.circle_grid_3x3_fill,
+                    size: 28.0,
+                  ),
+                ),
+                Positioned(
+                  right: 4.6,
+                  bottom: 11.0,
+                  child: Container(
+                    height: 16.0,
+                    width: 16.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Theme.of(context).accentColor,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
